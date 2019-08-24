b25d5923531c4bdf3b40a9838558aa1698e6d63e
Shamnad Shaji <shamnad.shaji@gofore.com>
2019-08-24 23:59:43 +0300

    styled the login page

--- scenes/Login.js
+++ scenes/Login.js
@@ -1,1 +1,41 @@
-import React, { Fragment } from 'react';
+import React, { Component } from 'react';
+import { Text, View, Button } from 'react-native';
+
+class Login extends Component {
+
+    render() {
+        return (
+            <View style={{
+                flex: 1,
+                backgroundColor: 'steelblue',
+                color: 'white'
+            }}>
+
+                <View style={{
+                    flex: 1, flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }} >
+                    <Button
+
+                        onPress={() => {
+                            alert('You tapped login!');
+                        }}
+                        title="Login"
+                        color="#FFFFFF"
+                    />
+                    <Button
+
+                        onPress={() => {
+                            alert('You tapped register!');
+                        }}
+                        title="Register"
+                        color="#FFFFFF"
+                    />
+                </View>
+            </View>
+        );
+    }
+}
+
+export default Login;
